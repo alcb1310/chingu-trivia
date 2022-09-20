7cdee02ebdacc27eb1c53f80681079558a4831b4
Andrés Court <andres@andrescourt.com>
2022-09-19 22:23:40 -0500

    Updates

---
+++ precache-manifest.a1bb147ede04e3593ec40e6a6a0c72b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b07362f8972e39d315f9",
+    "url": "/chingu-trivia/static/css/main.a8324efb.chunk.css"
+  },
+  {
+    "revision": "b07362f8972e39d315f9",
+    "url": "/chingu-trivia/static/js/main.b07362f8.chunk.js"
+  },
+  {
+    "revision": "ef8abe6476298281c2f8",
+    "url": "/chingu-trivia/static/css/1.0c106a95.chunk.css"
+  },
+  {
+    "revision": "ef8abe6476298281c2f8",
+    "url": "/chingu-trivia/static/js/1.ef8abe64.chunk.js"
+  },
+  {
+    "revision": "d1bae3e97733176c0d07",
+    "url": "/chingu-trivia/static/js/runtime~main.d1bae3e9.js"
+  },
+  {
+    "revision": "9447f4f75a56509b07074465cef80b7a",
+    "url": "/chingu-trivia/index.html"
+  }
+];
